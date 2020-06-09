0cd29ea7cb634136d85b4fe242866f3523491646
ziyad007 <ziyadloubaris@gmail.com>
2020-06-09 11:20:00 +0100

    [backend] optimisation de la recherche

--- FrontEnd/andromeda/src/containers/Testing/Testing.js
+++ FrontEnd/andromeda/src/containers/Testing/Testing.js
@@ -29,7 +29,7 @@ class Dashboard extends Component {
 
     getData = () => {
         this.setState({loading: true});
-        axios.get('http://localhost:8000/api/auth/user', this.props.user)
+        axios.get('http://localhost:8000/api/auth/search/autocomplete/a', this.props.user)
         .then(response => {
           console.log(response.data);
           this.setState({data:response.data});  
