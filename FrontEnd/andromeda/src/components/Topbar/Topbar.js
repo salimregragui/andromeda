e9006893965044192dc5cb3427f0f62805959dba
Your NameRegragui <salim.regragui27@gmail.com>
2020-06-18 03:49:32 +0100

    [FrontEnd] fixed theme toggle in topbar.

--- FrontEnd/andromeda/src/components/Topbar/Topbar.js
+++ FrontEnd/andromeda/src/components/Topbar/Topbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import classes from './Topbar.module.css';
 import SearchBar from '../../containers/SearchBar/SearchBar';
 import Notifications from '../../containers/Notifications/Notifications';
@@ -8,6 +8,12 @@ import {connect} from 'react-redux';
 import * as NotificationActions from '../../store/actions/index';
 
 const Topbar = (props) => {
+    const [sliderStyle, setSliderStyle] = useState(0);
+    
+    useEffect(() => {
+        setSliderStyle(localStorage.getItem('theme') === 'dark' ? classes.sliderDark : classes.slider);
+      }, []);
+
     let goToProfile = () => {
         props.history.push('/profile');
     }
@@ -15,6 +21,7 @@ const Topbar = (props) => {
     let toggleTheme = () => {
         if (localStorage.getItem('theme') === 'light') {
             localStorage.setItem('theme', 'dark');
+
             props.onAddNotification({
                 'type':'success',
                 'content':'Theme sombre activé avec succès il sera visible au prochain changement de page !',
@@ -23,6 +30,7 @@ const Topbar = (props) => {
             });
         }else {
             localStorage.setItem('theme', 'light');
+
             props.onAddNotification({
                 'type':'success',
                 'content':'Theme clair activé avec succès il sera visible au prochain changement de page !',
@@ -47,13 +55,7 @@ const Topbar = (props) => {
             {image}
         </div>
     }
-    let sliderStyle = null;
-    if (localStorage.getItem('theme') === 'dark') {
-        sliderStyle = classes.sliderDark;
-    }
-    else {
-        sliderStyle = classes.slider;
-    }
+    
     return (
         <header className={classes.Topbar} style = {localStorage.getItem('theme') === 'dark' ? {backgroundColor: '#2C2839', borderBottom: '1px solid #312C40'} : null}>
             <div className={classes.Logo} style = {localStorage.getItem('theme') === 'dark' ? {color: 'white', borderBottom: '1px solid #312C40'} : null}>
